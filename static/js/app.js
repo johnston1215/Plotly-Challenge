02e5ef2d93762b5e0faa76eab46e3b4accfd2839
Johnston1215 <johnston1215@yahoo.com>
2020-11-05 17:18:08 -0600

    11_05 pm

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,10 @@
-// Step 1: Plotly
-
 // 1. Use the D3 library to read in `samples.json`.
 function init() {
     d3.json("../samples.json").then(function(data) {
         console.log(data);
-        var s = data.samples;
+        // var s = data.samples;
         var n = data.names;
-        var m = data.metadata;
+        // var m = data.metadata;
         // console.log(n);
         // console.log(s);
         // console.log(m);
@@ -26,7 +24,7 @@ function optionChanged(sampleID) {
     var clear = d3.select("#sample-metadata");
     clear.html("");
     d3.json("../samples.json").then(function(data) {
-        var s = data.samples;
+        // var s = data.samples;
         var m = data.metadata;
         var targetMeta = m.filter(metadata => metadata.id == sampleID);
         console.log(targetMeta);
@@ -49,9 +47,9 @@ function optionChanged(sampleID) {
             var otuid = data.map(oi => oi.otu_ids);
             var sampVal = data.map(sv => sv.sample_values);
             var otulabel = data.map(ol => ol.otu_labels);
-            console.log(otuid);
-            console.log(sampVal);
-            console.log(otulabel);
+            // console.log(otuid);
+            // console.log(sampVal);
+            // console.log(otulabel);
             var trace1 = {
                 x: otuid[0],
                 y: sampVal[0],
@@ -61,30 +59,50 @@ function optionChanged(sampleID) {
                 text: otulabel[0]
             };
 
-            var barData = [trace1];
-            Plotly.newPlot("bubble", barData);
-            console.log(trace1)
+            var bubData = [trace1];
+            Plotly.newPlot("bubble", bubData);
+            // console.log(trace1)
         });
     };
     bplot();
+    //Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+    // * Use `sample_values` as the values for the bar chart.
+    // * Use `otu_ids` as the labels for the bar chart.
+    // * Use `otu_labels` as the hovertext for the chart.
+    function hplot(){
+        d3.json("../samples.json").then(function(data) {
+            data = data.samples.filter(otu => otu.id == sampleID);
+            var otuid = data.map(oi => oi.otu_ids);
+            var sampVal = data.map(sv => sv.sample_values);
+            var otulabel = data.map(ol => ol.otu_labels);
+            // console.log(otuid);
+            // console.log(sampVal);
+            // console.log(otulabel);
+            // var sorted = numArray.sort((a, b) => a - b);
+            var sorted = sampVal.sort((a,b) => a-b);
+            console.log(sorted);
+            // var sliced = sorted.slice(0, 10);
+            var sliced = sorted.slice(0,10);
+            console.log(sliced);
+
+            var trace2 = {
+                x: otuid[0],
+                y: sampVal[0],
+                type: "bar",
+                orientation: "h",
+                text: otulabel[0]
+            };
+
+            var barData = [trace2];
+            Plotly.newPlot("bar", barData);
+            // console.log(trace2)
+        });
+    };
+    hplot();
+
 };
 init();
-// Day 2 exercise 4 --  Data must be deleted before selecting another ID
 
-             //render bar chart
-// NewFunction, call optionChanged
-
-
-// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-
-// * Use `sample_values` as the values for the bar chart.
-// * Use `otu_ids` as the labels for the bar chart.
-// * Use `otu_labels` as the hovertext for the chart.
-
-// Create bar chart
-// numArray = [9.9, 6.1, 17.1, 22.7, 4.6, 8.7, 7.2];
-// var sorted = numArray.sort((a, b) => a - b);
-// var sliced = sorted.slice(0, 10);
 // console.log(sliced);
 // var trace1 = {
 //     type: "hbar",
@@ -96,7 +114,3 @@ init();
 //       color: "#17BECF"
 //     }
 //   };
-
-
-
-// 6. Update all of the plots any time that a new sample is selected;
